bc7e9111034dbc03127ea7790be066cd9e724e7e
pandekar <pandeadhistanaya@gmail.com>
2024-10-17 22:30:33 +0700

    refactor and tidy up code

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,47 @@
 export const COMPLETE_ITEM = 'COMPLETE_ITEM';
 
+const _createListItemTextElement = (todo) => {
+  const listItemTextElement = document.createElement('p');
+  listItemTextElement.innerText = todo.text;
+
+  if (todo.completed) {
+    listItemTextElement.setAttribute('class', 'completed');
+  }
+
+  return listItemTextElement;
+};
+
+const _createButtonSectionElement = (todos, todo) => {
+  const buttonSectionElement = document.createElement('div');
+  const buttonCompleteTodoElement = document.createElement('button');
+  const buttonDeleteTodoElement = document.createElement('button');
+
+  buttonCompleteTodoElement.addEventListener('click', () => {
+    todo.toggleCompleted();
+    
+    document.dispatchEvent(new Event(COMPLETE_ITEM));
+  });
+
+  if (todo.completed) {
+    buttonCompleteTodoElement.innerText = 'revert';
+    buttonCompleteTodoElement.setAttribute('class', 'button-revert');
+  } else {
+    buttonCompleteTodoElement.innerText = 'complete';
+    buttonCompleteTodoElement.setAttribute('class', 'button-complete');
+  }
+
+  buttonDeleteTodoElement.innerText = 'delete';
+  buttonDeleteTodoElement.addEventListener('click', () => {
+    todos.removeTodo(todo.id);
+  });
+
+  buttonSectionElement.setAttribute('class', 'button-section');
+  buttonSectionElement.appendChild(buttonCompleteTodoElement);
+  buttonSectionElement.appendChild(buttonDeleteTodoElement);
+
+  return buttonSectionElement;
+};
+
 export function renderTodos(todos) {
   // TODO: Implement renderTodos function
   const todoItems = todos.getTodos();
@@ -8,41 +50,13 @@ export function renderTodos(todos) {
 
   for (const index in todoItems) {
     const todo = todoItems[index];
+
     const listItemElement = document.createElement('li');
-    const listItemTextElement = document.createElement('p');
-    const buttonSectionElement = document.createElement('div');
-    const buttonCompleteTodoElement = document.createElement('button');
-    const buttonDeleteTodoElement = document.createElement('button');
+    const listItemTextElement = _createListItemTextElement(todo);
+    const buttonSectionElement = _createButtonSectionElement(todos, todo);
 
     listItemElement.setAttribute('id', todo.id);
     listItemElement.setAttribute('class', 'todo-item');
-    listItemTextElement.innerText = todo.text;
-    if (todo.completed) {
-      listItemTextElement.setAttribute('class', 'completed');
-    }
-
-    buttonCompleteTodoElement.addEventListener('click', () => {
-      todo.toggleCompleted();
-      
-      document.dispatchEvent(new Event(COMPLETE_ITEM));
-    });
-    if (todo.completed) {
-      buttonCompleteTodoElement.innerText = 'uncomplete';
-      buttonCompleteTodoElement.setAttribute('class', 'button-uncomplete');
-    } else {
-      buttonCompleteTodoElement.innerText = 'complete';
-      buttonCompleteTodoElement.setAttribute('class', 'button-complete');
-    }
-
-    buttonDeleteTodoElement.innerText = 'delete';
-    buttonDeleteTodoElement.addEventListener('click', () => {
-      todos.removeTodo(todo.id);
-    });
-
-    buttonSectionElement.setAttribute('class', 'button-section');
-    buttonSectionElement.appendChild(buttonCompleteTodoElement);
-    buttonSectionElement.appendChild(buttonDeleteTodoElement);
-
     listItemElement.appendChild(listItemTextElement);
     listItemElement.appendChild(buttonSectionElement);
 
